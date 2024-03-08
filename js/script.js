9db6262e1c9533d30cd3e8b9e956941429c5e8b2
Aligern <sasso.luca93@gmail.com>
2024-03-08 16:07:53 +0100

    creating a div into HTML from Js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
+
+// we create from js a container into our HTML
+const boxsquare = document.createElement('div');
+
+
 // we create a for loop that need to count untill 100
 for (let i = 0; i < 100; i++) {
     // if i is % for 15 the result must be a FizzBuzz
-    if (i % 15 === 0)
-    console.log("fizzbuzz");
+    if (i % 15 === 0) {
+        console.log("fizzbuzz");
+    }
     // if i is % for 3 the result must be a Fizz
-    else if (i % 3 === 0)
-    console.log("fizz");
+    else if (i % 3 === 0) {
+        console.log("fizz");
+    }
     // if i is % for 5 the result must be a Buzz
-    else if (i % 5 === 0)
-    console.log("buzz");
-    else
-    console.log(i);
+    else if (i % 5 === 0) {
+        console.log("buzz");
+    }
+    else {
+        console.log(i);
+    }
 }
+
