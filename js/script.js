5ad08aa99e865c5da345faf2d99e546b011a5d6d
Aligern <sasso.luca93@gmail.com>
2024-03-08 16:57:36 +0100

    getting closer to the solution

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const boxsquare = document.createElement('div');
 // console.log(boxsquare);
 
 //we give our div the classes we want
-boxsquare.className = 'd-flex justify-content-between align-content-center gap-2';
+boxsquare.className = 'd-flex justify-content-between align-content-center flex-wrap';
 // also add ID to the div
 boxsquare.setAttribute('id','ls-container');
 // console.log(boxsquare);
@@ -15,16 +15,20 @@ boxsquare.innerHTML = tmpHtml;
 
 // we create a for loop that need to count untill 100
 for (let i = 1; i <= numBox; i++) {
+    let num = 0;
     // if i is % for 15 the result must be a FizzBuzz
     if (i % 15 === 0) {
+        tmpHtml += `<div class="box fizzbuzz">${i + 1}</div>`;
         console.log("fizzbuzz");
     }
     // if i is % for 3 the result must be a Fizz
     else if (i % 3 === 0) {
+        tmpHtml += `<div class="box fizz">${i + 1}</div>`;
         console.log("fizz");
     }
     // if i is % for 5 the result must be a Buzz
     else if (i % 5 === 0) {
+        tmpHtml += `<div class="box buzz">${i + 1}</div>`;
         console.log("buzz");
     }
     else {
