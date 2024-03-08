68bd5ad59e7cd782780983074717e754f4123154
Aligern <sasso.luca93@gmail.com>
2024-03-08 16:43:34 +0100

    another commit

--- js/script.js
+++ js/script.js
@@ -2,11 +2,19 @@
 const boxsquare = document.createElement('div');
 // console.log(boxsquare);
 
-boxsquare.className = 'd-flex justify-content-center align-content-center gap-2';
-console.log(boxsquare)
+//we give our div the classes we want
+boxsquare.className = 'd-flex justify-content-between align-content-center gap-2';
+// also add ID to the div
+boxsquare.setAttribute('id','ls-container');
+// console.log(boxsquare);
+
+let numBox = 100;
+let tmpHtml = '';
+
+boxsquare.innerHTML = tmpHtml;
 
 // we create a for loop that need to count untill 100
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= numBox; i++) {
     // if i is % for 15 the result must be a FizzBuzz
     if (i % 15 === 0) {
         console.log("fizzbuzz");
